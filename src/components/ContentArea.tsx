6a7c4b2e231680128eccebc3684004877705d116
nurzat <nurzat404@gmail.com>
2022-09-20 11:41:40 +0800

    create contentarea

---
+++ src/components/ContentArea.tsx
@@ -0,0 +1,15 @@
+function ContentArea() {
+    async function greet() {
+    }
+
+    return (
+        <>
+            <input />
+            <button onClick={() => greet()}>
+                Greet
+            </button>
+        </>
+    );
+}
+
+export default ContentArea;
