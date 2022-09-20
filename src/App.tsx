e1d57782d9b5da62cb78fd2cba90215ea5a8ce0d
nurzat <nurzat404@gmail.com>
2022-09-20 11:48:17 +0800

    remove redundant content

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import ContentArea from "./components/ContentArea";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet"));
-  }
-
   return (
-    <div className="container">
-      <div className="row">
-        <div>
-          <input
-            onChange={(e) => setName(e.currentTarget.value)}
-          />
-          <button onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-    </div>
+        <>
+          <ContentArea/>
+        </>
   );
 }
 
